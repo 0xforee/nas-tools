17ce589fd8997c8f9ea4c8d1a607e067e3b099fc
foree <daniel2008lee@163.com>
2024-10-13 14:05:34 +0800

    fix error

--- app/utils/time_utils.py
+++ app/utils/time_utils.py
@@ -3,25 +3,33 @@ from datetime import datetime
 class TimeUtils:
     @staticmethod
     def time_difference(last_seen_str):
-        # 将时间字符串解析为 datetime 对象
-        last_seen = datetime.strptime(last_seen_str, "%Y-%m-%d %H:%M:%S")
-        current_time = datetime.now()
 
-        # 计算时间差
-        time_diff = current_time - last_seen
+        try:
+            if not last_seen_str:
+                return ""
+            if not isinstance(last_seen_str, str):
+                return ""
+            # 将时间字符串解析为 datetime 对象
+            last_seen = datetime.strptime(last_seen_str, "%Y-%m-%d %H:%M:%S")
+            current_time = datetime.now()
 
-        # 获取天、小时和分钟
-        days = time_diff.days
-        hours, remainder = divmod(time_diff.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
+            # 计算时间差
+            time_diff = current_time - last_seen
 
-        # 构建返回结果
-        result_parts = []
-        if days > 0:
-            result_parts.append(f"{days}天")
-        if hours > 0:
-            result_parts.append(f"{hours}时")
-        if minutes > 0 or (days == 0 and hours == 0):  # 如果天和小时都为0，仍然显示分钟
-            result_parts.append(f"{minutes}分")
+            # 获取天、小时和分钟
+            days = time_diff.days
+            hours, remainder = divmod(time_diff.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
 
-        return ''.join(result_parts) + "前"
+            # 构建返回结果
+            result_parts = []
+            if days > 0:
+                result_parts.append(f"{days}天")
+            if hours > 0:
+                result_parts.append(f"{hours}时")
+            if minutes > 0 or (days == 0 and hours == 0):  # 如果天和小时都为0，仍然显示分钟
+                result_parts.append(f"{minutes}分")
+
+            return ''.join(result_parts) + "前"
+        except:
+            return ''
